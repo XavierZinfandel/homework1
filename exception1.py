a638346ff0fc90c508e4eef8936ef749a4bbc811
XavierZinfandel <65195285+XavierZinfandel@users.noreply.github.com>
2020-05-28 05:07:06 +1000

    Update exception1.py

--- exception1.py
+++ exception1.py
@@ -3,7 +3,7 @@ def get_summ(num_one, num_two):
         result = num_one + num_two
         return int(result)
     except (TypeError, ValueError):
-        print("Введите число") 
+        return ("Введите число") 
    
 if __name__ == "__main__":
     print(get_summ(2, 2))
